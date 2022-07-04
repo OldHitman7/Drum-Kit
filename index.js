d622f2e9f1f1c657a7169a7062ceaab869b85f33
OldHitman7 <farisbinasif7@gmail.com>
2022-07-04 11:29:07 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+var n = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < n; i++) {
+
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+
+    var buttonInnerHTML = this.innerHTML;
+    var audio;
+    switch (buttonInnerHTML) {
+      case "w":
+        audio = new Audio("sounds/tom-1.mp3");
+        audio.play();
+        break;
+      case "a":
+        audio = new Audio("sounds/tom-2.mp3");
+        audio.play();
+        break;
+      case "s":
+        audio = new Audio("sounds/tom-3.mp3");
+        audio.play();
+        break;
+      case "d":
+        audio = new Audio("sounds/tom-4.mp3");
+        audio.play();
+        break;
+      case "j":
+        audio = new Audio("sounds/snare.mp3");
+        audio.play();
+        break;
+      case "k":
+        audio = new Audio("sounds/crash.mp3");
+        audio.play();
+        break;
+      case "l":
+        audio = new Audio("sounds/kick-bass.mp3");
+        audio.play();
+        break;
+      default:
+
+    }
+
+  });
+
+}
